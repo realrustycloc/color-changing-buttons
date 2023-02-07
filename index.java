af20f54b5c4eb1b0fb9828ecb4d742aa781adfb7
rustycloc <124487848+realrustycloc@users.noreply.github.com>
2023-02-06 20:33:26 -0500

    Add files via upload

---
+++ index.java
@@ -0,0 +1,14 @@
+const name = document.querySelector(".name")
+
+function ChangeToBlue() {
+          name.style.color = "blue";
+
+}
+
+function ChangeToRed() {
+          name.style.color = "red";
+}
+
+function ChangeToGreen() {
+          name.style.color = "lightGreen";
+}
